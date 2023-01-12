545bccc08e59c1820e1f849acedee897908317d8
Jianmin Wei <jianminwei1@gmail.com>
2023-01-12 06:30:10 -0500

    initial commit

---
+++ Java/CoreJava/src/grouplearning/corejava/basics/classes/TestPerson.java
@@ -0,0 +1,20 @@
+package grouplearning.corejava.basics.classes;
+
+public class TestPerson {
+    public static void main(String[] args) {
+        Person p = new Person();
+
+        p.setName("Matthew");
+        p.setAge(24);
+        //Matthew's age is 24
+
+        Person p2 = new Person();
+        p2.setName("Kyle");
+        p2.setAge(20);
+
+
+        System.out.println("Matthew's age is " + p.getAge());
+        System.out.println("Kyle's age is " + p2.getAge());
+    }
+}
+
