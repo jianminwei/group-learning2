4577274d3f10183ef85c6454b75931cdea765763
Matthew Wei <weimatthewj@gmail.com>
2023-01-17 19:26:23 -0500

    created car class 1/17/23

---
+++ Java/CoreJava/src/grouplearning/corejava/basics/classes/Car.java
@@ -0,0 +1,22 @@
+package grouplearning.corejava.basics.classes;
+
+public class Car {
+    private String model;
+    private int year;
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
