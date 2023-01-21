edbc00a48e0accec73fcdedc95c886c3cd69d93b
Jianmin Wei <jianminwei1@gmail.com>
2023-01-21 05:36:50 -0500

    Jan 21, 2023, added some comments and pointers

--- Java/CoreJava/src/grouplearning/corejava/basics/class_constructor/Cat.java
+++ Java/CoreJava/src/grouplearning/corejava/basics/class_constructor/Cat.java
@@ -2,8 +2,9 @@ package grouplearning.corejava.basics.class_constructor;
 
 /**
  * By default, Java will create a no argument constructor for you.
- * However, if you define any constructors, Java is not going to create the default constructor anymore.
- * You would have to create on your own if you still need that no argument constructor.
+ * However, if you define any constructors, Java is not going to create the default no argument constructor anymore.
+ * You would have to create your own no argument constructor if you still need that no argument constructor
+ * in addition to other constructors.
  */
 public class Cat {
 
--- Java/CoreJava/src/grouplearning/corejava/basics/overriding/overriding3/Circle.java
+++ Java/CoreJava/src/grouplearning/corejava/basics/overriding/overriding3/Circle.java
@@ -4,6 +4,11 @@ public class Circle extends Shape{
 
     private int radius;
 
+    /**
+     * By convention, when you extends an abstract class,
+     * you also use "@Override" annotation for the implemented method. But if you
+     * ignore this annotation, it still works.
+     */
     @Override
     public double area(int r) {
         return 3.14 * r * r;
