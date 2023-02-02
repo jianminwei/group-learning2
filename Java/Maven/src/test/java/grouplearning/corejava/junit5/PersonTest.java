7868c2f841c5275f1297385e2170cd1673e3c04f
Jianmin Wei <jianminwei1@gmail.com>
2023-02-01 20:22:50 -0500

    commited after PersonTest

--- Java/Maven/src/test/java/grouplearning/corejava/junit5/PersonTest.java
+++ Java/Maven/src/test/java/grouplearning/corejava/junit5/PersonTest.java
@@ -1,19 +1,12 @@
 package grouplearning.corejava.junit5;
 
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonTest {
 
-    private Person person = new Person();
-
-//    @BeforeEach
-//    void setup() {
-//        person = new Person();
-//    }
+    Person person = new Person();
 
     @Test
     void getFirstName() {
@@ -21,17 +14,20 @@ class PersonTest {
         assertEquals("Foo", person.getFirstName());
     }
 
+
     @Test
     void getLastName() {
         person.setLastName("Bar");
         assertEquals("Bar", person.getLastName());
     }
 
+
     @Test
     void getFullName() {
+
         person.setFirstName("Foo");
         person.setLastName("Bar");
         assertEquals("Foo Bar", person.getFullName());
-    }
 
+    }
 }
